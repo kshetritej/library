0f03c1f9b86acd900f68880918696025c9dd1e56
kshetritej <furiousdtej@gmail.com>
2023-05-09 13:44:12 +0545

    create dynamic card using js on adding item

--- script.js
+++ script.js
@@ -16,11 +16,13 @@ const addBookBtn = document.querySelector(".add-a-book-btn");
 const addBookForm = document.querySelector(".add-book-form");
 const form = document.querySelector(".form");
 
-addBookBtn.addEventListener("click", () => {
+addBookBtn.addEventListener("click", (e) => {
+  e.preventDefault();
   addBookForm.style.display = "block";
   form.style.display = "block";
 });
 
+// Get Input from Form
 const submitBtn = document.querySelector(".submit-button");
 submitBtn.addEventListener("click",(e)=>{
   e.preventDefault();
@@ -38,8 +40,70 @@ submitBtn.addEventListener("click",(e)=>{
 
   addBookToLibrary(title,author,status,pages);
   form.style.display = "none";
+  displayBook(Book);
 });
 
-// Get Input from Form
+// Display card on book add
+const container = document.querySelector(".container");
+
+function displayBook(Book){
+  const bookCard = document.createElement("div");
+  bookCard.className = "book";
+  container.appendChild(bookCard);
+
+  const bookTitle = document.createElement("div");
+  bookTitle.className = "book-title";
+  bookCard.appendChild(bookTitle);
+  bookTitle.textContent = Book.title;
+
+  const bookDetails = document.createElement("div");
+  bookDetails.className = "book-details";
+  bookCard.appendChild(bookDetails);
+
+  // Author Details
+  const authorDetail = document.createElement("div");
+  authorDetail.className = "author detail";
+  bookDetails.appendChild(authorDetail);
+
+  const authorHero = document.createElement("div");
+  authorHero.className = "author-hero";
+  authorDetail.appendChild(authorHero);
+  authorHero.textContent = "Author";
+
+  const author = document.createElement("div");
+  author.classNae = "author";
+  authorDetail.appendChild(author);
+  author.textContent = Book.author;
+
+  // Status Detail
+  const statusDetail = document.createElement("div");
+  statusDetail.className = "status detail";
+  bookDetails.appendChild(statusDetail);
+
+  const statusHero = document.createElement("div");
+  statusHero.className = "status-hero";
+  statusDetail.appendChild(statusHero);
+  statusHero.textContent = "Status";
+
+  const status = document.createElement("div");
+  status.classNae = "what-status";
+  statusDetail.appendChild(status);
+  status.textContent = Book.status;
+
+  // Pages Details
+  const pagesDetail = document.createElement("div");
+  pagesDetail.className = "pages detail";
+  bookDetails.appendChild(pagesDetail);
+
+  const pagesHero = document.createElement("div");
+  pagesHero.className = "pages-hero";
+  pagesDetail.appendChild(pagesHero);
+  pagesHero.textContent = "Pages";
+
+  const pages = document.createElement("div");
+  pages.classNae = "page-nos";
+  pagesDetail.appendChild(pages);
+  pages.textContent = Book.pages;
+}
 
 

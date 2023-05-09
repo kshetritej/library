67492312dd5ff33213622b94ecb67428a2b02437
kshetritej <furiousdtej@gmail.com>
2023-05-09 14:03:38 +0545

    Add book, and remove button added * Function for Adding book works now, * Add option for removing books from list, and style the button,

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function Book(title,author,status,pages){
   this.status = status;
   this.pages = pages;
 }
+
 function addBookToLibrary(title, author, status, pages) {
   const newBook = new Book(title,author,status,pages);
   library.push(newBook);
@@ -40,13 +41,13 @@ submitBtn.addEventListener("click",(e)=>{
 
   addBookToLibrary(title,author,status,pages);
   form.style.display = "none";
-  displayBook(Book);
+  displayBook(library.length -1);
 });
 
 // Display card on book add
 const container = document.querySelector(".container");
 
-function displayBook(Book){
+function displayBook(index){
   const bookCard = document.createElement("div");
   bookCard.className = "book";
   container.appendChild(bookCard);
@@ -54,7 +55,7 @@ function displayBook(Book){
   const bookTitle = document.createElement("div");
   bookTitle.className = "book-title";
   bookCard.appendChild(bookTitle);
-  bookTitle.textContent = Book.title;
+  bookTitle.textContent = library[index].title;
 
   const bookDetails = document.createElement("div");
   bookDetails.className = "book-details";
@@ -73,7 +74,7 @@ function displayBook(Book){
   const author = document.createElement("div");
   author.classNae = "author";
   authorDetail.appendChild(author);
-  author.textContent = Book.author;
+  author.textContent = library[index].author;
 
   // Status Detail
   const statusDetail = document.createElement("div");
@@ -88,7 +89,7 @@ function displayBook(Book){
   const status = document.createElement("div");
   status.classNae = "what-status";
   statusDetail.appendChild(status);
-  status.textContent = Book.status;
+  status.textContent = library[index].status;
 
   // Pages Details
   const pagesDetail = document.createElement("div");
@@ -103,7 +104,13 @@ function displayBook(Book){
   const pages = document.createElement("div");
   pages.classNae = "page-nos";
   pagesDetail.appendChild(pages);
-  pages.textContent = Book.pages;
+  pages.textContent = library[index].pages;
+
+  const removeBook = document.createElement("div");
+  removeBook.className ="remove-book";
+  removeBook.textContent = "Remove";
+  removeBook.setAttribute("style","")
+  bookCard.appendChild(removeBook);
 }
 
 

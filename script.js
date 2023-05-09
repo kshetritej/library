9cce61e285b63dd3e0445c3ec54f63aa20ce4e0d
kshetritej <furiousdtej@gmail.com>
2023-05-09 12:31:30 +0545

    can log inputs in console

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function Book(title, author, status, pages) {
+ let library = [];
+
+function Book(title,author,status,pages){
   this.title = title;
   this.author = author;
   this.status = status;
   this.pages = pages;
+}
+function addBookToLibrary(title, author, status, pages) {
+  const newBook = new Book(title,author,status,pages);
+  library.push(newBook);
+  console.log(library);
 }
 
 const addBookBtn = document.querySelector(".add-a-book-btn");
@@ -15,7 +22,24 @@ addBookBtn.addEventListener("click", () => {
 });
 
 const submitBtn = document.querySelector(".submit-button");
-submitBtn.addEventListener("click",()=>{
+submitBtn.addEventListener("click",(e)=>{
+  e.preventDefault();
+  const titleName = document.getElementById("book-title");
+  const title = titleName.value;
+
+  const authorName = document.getElementById("author");
+  const author = authorName.value;
+
+  const statusState = document.getElementById("status");
+  const status = statusState.value;
+
+  const pagesCount = document.getElementById("page");
+  const pages = pagesCount.value;
+
+  addBookToLibrary(title,author,status,pages);
   form.style.display = "none";
-})
+});
+
+// Get Input from Form
+
 

ff8199d0b26749084d7dcd061b5bbfe1734e3e45
kshetritej <furiousdtej@gmail.com>
2023-05-10 13:49:47 +0545

    can change read status

--- script.js
+++ script.js
@@ -109,6 +109,58 @@ function displayBook(index) {
     status.style.background = "#ffe200";
   }
 
+  // Change Status
+
+
+  status.addEventListener('click',()=>{
+    if(status.textContent == "Not Started"){
+      status.textContent = "In Progress";
+      status.style.background = "#ffe200";
+    }
+    else if(status.textContent == "In Progress"){
+      status.textContent = "Completed";
+      status.style.background = "#90ee90";
+    }
+    else{
+      status.textContent = "Not Started";
+      status.style.background = "#f08080";
+    }
+  // const dropDown = document.createElement("div");
+  // let isDropdownOpen = false;
+  //   dropDown.className = "status-dropdown";
+  //   status.appendChild(dropDown);
+
+  //   dropDown.setAttribute("style","display:flex;flex-direction:column;background: #fff;padding: 0.2rem;")
+
+  //   const notReadOption = document.createElement("div");
+  //   notReadOption.className = "not-read option";
+  //   notReadOption.textContent = "Not Started";
+  //   dropDown.appendChild(notReadOption);
+    
+  //   const inProgress = document.createElement("div");
+  //   inProgress.className = "in-progress option";
+  //   inProgress.textContent = "In Progress";
+  //   dropDown.appendChild(inProgress);
+  
+  //   const completed = document.createElement("div");
+  //   completed.className = "completed option";
+  //   completed.textContent = "Completed";
+  //   dropDown.appendChild(completed);
+    
+  // },
+  // {once: true},);
+
+  // const options = document.querySelectorAll(".option");
+  // options.forEach(option => {
+  //   option.addEventListener("click",()=>{
+  //     status.textContent = option.textContent;
+  //     dropDown.style.display = "none";
+  //   }) 
+  // !Will work on this complex behavior later.
+});
+
+
+
   // Pages Details
   const pagesDetail = document.createElement("div");
   pagesDetail.className = "pages detail";

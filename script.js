f07666bdd7411ea0ac136d3b3f6653bfe72c06ab
kshetritej <furiousdtej@gmail.com>
2023-05-10 14:16:52 +0545

    add form clearence on submit

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ let dummy = {
   pages: 299,
 }
 library.push(dummy);
-console.log(library)
 displayBook(library.length - 1);
 
 function Book(title, author, status, pages) {
@@ -49,6 +48,12 @@ submitBtn.addEventListener("click", (e) => {
   const pages = pagesCount.value;
 
   addBookToLibrary(title, author, status, pages);
+
+  // Clear Input Fields once Inputs are submitted
+  const Inputs = document.querySelectorAll("input");
+  Inputs.forEach(input => {
+    input.value = "";
+  });
   form.style.display = "none";
   displayBook(library.length - 1);
 });
@@ -79,7 +84,7 @@ function displayBook(index) {
   authorHero.textContent = "Author";
 
   const author = document.createElement("div");
-  author.className= "author";
+  author.className = "author";
   authorDetail.appendChild(author);
   author.textContent = library[index].author;
 
@@ -112,54 +117,54 @@ function displayBook(index) {
   // Change Status
 
 
-  status.addEventListener('click',()=>{
-    if(status.textContent == "Not Started"){
+  status.addEventListener('click', () => {
+
+    console.log(library);
+    if (status.textContent == "Not Started") {
       status.textContent = "In Progress";
       status.style.background = "#ffe200";
     }
-    else if(status.textContent == "In Progress"){
+    else if (status.textContent == "In Progress") {
       status.textContent = "Completed";
       status.style.background = "#90ee90";
     }
-    else{
+    else {
       status.textContent = "Not Started";
       status.style.background = "#f08080";
     }
-  // const dropDown = document.createElement("div");
-  // let isDropdownOpen = false;
-  //   dropDown.className = "status-dropdown";
-  //   status.appendChild(dropDown);
+    // const dropDown = document.createElement("div");
+    // let isDropdownOpen = false;
+    //   dropDown.className = "status-dropdown";
+    //   status.appendChild(dropDown);
 
-  //   dropDown.setAttribute("style","display:flex;flex-direction:column;background: #fff;padding: 0.2rem;")
+    //   dropDown.setAttribute("style","display:flex;flex-direction:column;background: #fff;padding: 0.2rem;")
 
-  //   const notReadOption = document.createElement("div");
-  //   notReadOption.className = "not-read option";
-  //   notReadOption.textContent = "Not Started";
-  //   dropDown.appendChild(notReadOption);
-    
-  //   const inProgress = document.createElement("div");
-  //   inProgress.className = "in-progress option";
-  //   inProgress.textContent = "In Progress";
-  //   dropDown.appendChild(inProgress);
-  
-  //   const completed = document.createElement("div");
-  //   completed.className = "completed option";
-  //   completed.textContent = "Completed";
-  //   dropDown.appendChild(completed);
-    
-  // },
-  // {once: true},);
+    //   const notReadOption = document.createElement("div");
+    //   notReadOption.className = "not-read option";
+    //   notReadOption.textContent = "Not Started";
+    //   dropDown.appendChild(notReadOption);
 
-  // const options = document.querySelectorAll(".option");
-  // options.forEach(option => {
-  //   option.addEventListener("click",()=>{
-  //     status.textContent = option.textContent;
-  //     dropDown.style.display = "none";
-  //   }) 
-  // !Will work on this complex behavior later.
-});
+    //   const inProgress = document.createElement("div");
+    //   inProgress.className = "in-progress option";
+    //   inProgress.textContent = "In Progress";
+    //   dropDown.appendChild(inProgress);
 
+    //   const completed = document.createElement("div");
+    //   completed.className = "completed option";
+    //   completed.textContent = "Completed";
+    //   dropDown.appendChild(completed);
 
+    // },
+    // {once: true},);
+
+    // const options = document.querySelectorAll(".option");
+    // options.forEach(option => {
+    //   option.addEventListener("click",()=>{
+    //     status.textContent = option.textContent;
+    //     dropDown.style.display = "none";
+    //   }) 
+    // !Will work on this complex behavior later.
+  });
 
   // Pages Details
   const pagesDetail = document.createElement("div");
